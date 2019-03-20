7977d52099d5ad9bfdbdeee2a5d91c5b5170594c
Andreas Faust <>
2019-03-20 17:47:10 +0100

    Test Changelog II.

--- example/src/App.js
+++ example/src/App.js
@@ -6,7 +6,7 @@ export default class App extends Component {
   render () {
     return (
       <div>
-        <ExampleComponent text='Test!' />
+        <ExampleComponent text='Test!!!!' />
       </div>
     )
   }
