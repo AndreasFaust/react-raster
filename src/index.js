a8fd73513b9c3ac6bcc8de9619ec8051bf9c3af9
Andreas Faust <>
2019-03-20 17:41:53 +0100

    Initializing library.

--- src/index.js
+++ src/index.js
@@ -4,18 +4,14 @@ import PropTypes from 'prop-types'
 import styles from './styles.css'
 
 export default class ExampleComponent extends Component {
-  static propTypes = {
-    text: PropTypes.string
-  }
-
-  render() {
+  render () {
     const {
       text
     } = this.props
 
     return (
       <div className={styles.test}>
-        Example Component: {text}
+        Example: {text}
       </div>
     )
   }
