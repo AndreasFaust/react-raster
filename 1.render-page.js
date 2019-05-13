3257d38c0348146e15714e27089159ce76a7254e
Andreas Faust <>
2019-05-13 03:00:27 +0200

    Updates

---
+++ 1.render-page.js
@@ -0,0 +1,83 @@
+exports.ids = [1];
+exports.modules = {
+
+/***/ "./node_modules/docz/dist/Playground.esm.js":
+/*!**************************************************!*\
+  !*** ./node_modules/docz/dist/Playground.esm.js ***!
+  \**************************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "react");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var lodash_fp_get__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! lodash/fp/get */ "lodash/fp/get");
+/* harmony import */ var lodash_fp_get__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(lodash_fp_get__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var _chunk_esm_js__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./chunk.esm.js */ "./node_modules/docz/dist/chunk.esm.js");
+/* harmony import */ var lodash_fp_omit__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! lodash/fp/omit */ "lodash/fp/omit");
+/* harmony import */ var lodash_fp_omit__WEBPACK_IMPORTED_MODULE_3___default = /*#__PURE__*/__webpack_require__.n(lodash_fp_omit__WEBPACK_IMPORTED_MODULE_3__);
+/* harmony import */ var fast_deep_equal__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! fast-deep-equal */ "./node_modules/fast-deep-equal/index.js");
+/* harmony import */ var fast_deep_equal__WEBPACK_IMPORTED_MODULE_4___default = /*#__PURE__*/__webpack_require__.n(fast_deep_equal__WEBPACK_IMPORTED_MODULE_4__);
+/* harmony import */ var lodash_fp_merge__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! lodash/fp/merge */ "lodash/fp/merge");
+/* harmony import */ var lodash_fp_merge__WEBPACK_IMPORTED_MODULE_5___default = /*#__PURE__*/__webpack_require__.n(lodash_fp_merge__WEBPACK_IMPORTED_MODULE_5__);
+/* harmony import */ var array_sort__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(/*! array-sort */ "./node_modules/array-sort/index.js");
+/* harmony import */ var array_sort__WEBPACK_IMPORTED_MODULE_6___default = /*#__PURE__*/__webpack_require__.n(array_sort__WEBPACK_IMPORTED_MODULE_6__);
+/* harmony import */ var lodash_fp_unionBy__WEBPACK_IMPORTED_MODULE_7__ = __webpack_require__(/*! lodash/fp/unionBy */ "lodash/fp/unionBy");
+/* harmony import */ var lodash_fp_unionBy__WEBPACK_IMPORTED_MODULE_7___default = /*#__PURE__*/__webpack_require__.n(lodash_fp_unionBy__WEBPACK_IMPORTED_MODULE_7__);
+/* harmony import */ var lodash_fp_flattenDepth__WEBPACK_IMPORTED_MODULE_8__ = __webpack_require__(/*! lodash/fp/flattenDepth */ "lodash/fp/flattenDepth");
+/* harmony import */ var lodash_fp_flattenDepth__WEBPACK_IMPORTED_MODULE_8___default = /*#__PURE__*/__webpack_require__.n(lodash_fp_flattenDepth__WEBPACK_IMPORTED_MODULE_8__);
+/* harmony import */ var lodash_fp_pipe__WEBPACK_IMPORTED_MODULE_9__ = __webpack_require__(/*! lodash/fp/pipe */ "lodash/fp/pipe");
+/* harmony import */ var lodash_fp_pipe__WEBPACK_IMPORTED_MODULE_9___default = /*#__PURE__*/__webpack_require__.n(lodash_fp_pipe__WEBPACK_IMPORTED_MODULE_9__);
+/* harmony import */ var ulid__WEBPACK_IMPORTED_MODULE_10__ = __webpack_require__(/*! ulid */ "./node_modules/ulid/dist/index.esm.js");
+/* harmony import */ var match_sorter__WEBPACK_IMPORTED_MODULE_11__ = __webpack_require__(/*! match-sorter */ "./node_modules/match-sorter/dist/match-sorter.esm.js");
+/* harmony import */ var lodash_fp_throttle__WEBPACK_IMPORTED_MODULE_12__ = __webpack_require__(/*! lodash/fp/throttle */ "lodash/fp/throttle");
+/* harmony import */ var lodash_fp_throttle__WEBPACK_IMPORTED_MODULE_12___default = /*#__PURE__*/__webpack_require__.n(lodash_fp_throttle__WEBPACK_IMPORTED_MODULE_12__);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const Playground = ({
+  className,
+  style,
+  wrapper: Wrapper,
+  children,
+  __scope,
+  __position,
+  __code,
+  __codesandbox
+}) => {
+  const components = Object(_chunk_esm_js__WEBPACK_IMPORTED_MODULE_2__["b"])();
+  if (!components || !components.playground) return null;
+  const props = {
+    className,
+    style,
+    components
+  };
+  return Object(react__WEBPACK_IMPORTED_MODULE_0__["createElement"])(components.playground, Object.assign({}, props, {
+    component: children,
+    wrapper: Wrapper,
+    scope: __scope,
+    position: __position,
+    code: __code,
+    codesandbox: __codesandbox
+  }));
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (Playground);
+
+
+/***/ })
+
+};;
+//# sourceMappingURL=1.render-page.js.map
