8d2a6c96b44f1abda6fe1481df0e430ee8bad7c1
Andreas Faust <>
2019-10-27 02:23:17 +0200

    delete console.log

--- src/raster/Grid/index.js
+++ src/raster/Grid/index.js
@@ -37,7 +37,7 @@ const Grid = React.forwardRef((props, ref) => {
     attrs,
     isControl
   } = props
-  console.log(cssMode)
+
   const controlIsVisible = useControl(control)
   const cssModeNormalized = useCssMode(cssMode || 'grid')
 
