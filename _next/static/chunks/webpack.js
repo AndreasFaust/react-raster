d1789daf542779ab317d3a6aa26d59ed94310243
Andreas Faust <info@andreasfaust.de>
2021-01-17 16:21:08 +0100

    Updates

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "64a619ceb534bc8f6bce";
+/******/ 	var hotCurrentHash = "3583e3a86ad97a833b9e";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
